421a0c7dd14d433a295203df630f2b41488dd71c
tenacious <tenacioustommy6@gmail.com>
2024-03-20 15:56:53 +0800

    update dir

--- project/12306.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-option=Options()
-option.add_argument('--disable-blink-features=AutomationControlled')
-web=Chrome(options=option)
-web.get('https://kyfw.12306.cn/otn/resources/login.html')
-
